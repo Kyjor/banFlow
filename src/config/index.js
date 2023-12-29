707ba38b2bc7ca6efb1d8e01db1423101680d4dc
Kyle Conel <kyjordev@gmail.com>
2023-12-29 17:37:26 -0500

    Auto stash before merge of "main" and "feature/packageupdates"

--- src/config/index.js
+++ src/config/index.js
@@ -5,9 +5,6 @@ const URL_API = 'http://localhost:8000';
 const KEY_JWT = 'ABC123456';
 
 // Example Images
-const IMAGES = {
-  // eslint-disable-next-line global-require
-  LOGO: require('../assets/images/logo.png'),
-};
+const IMAGES = {};
 
 export { URL_API, KEY_JWT, IMAGES };
